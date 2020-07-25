542c58eda96d7422af8911f0202477487c2f6269
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-07-25 16:03:34 -0700

    Initial Commit

---
+++ Knot VisualizerTests/Knot_VisualizerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Knot_VisualizerTests.swift
+//  Knot VisualizerTests
+//
+//  Created by Karthik Dharmarajan on 7/25/20.
+//  Copyright © 2020 Karthik Dharmarajan. All rights reserved.
+//
+
+import XCTest
+@testable import Knot_Visualizer
+
+class Knot_VisualizerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
